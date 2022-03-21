6c9ce3e130e483674b50a09ef9e1f8c015a9e7aa
HuiyuGuan <Huiyu.guan20@bcmail.cuny.edu>
2022-03-20 23:05:41 -0400

    upload the instagram app login

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "E2GXhwdM8fyzegDSEpCUJDpAT9Rl5VW0WomnJRa5" 
+                $0.clientKey = "aiTXia6E35wkwR2KwoxbIcTSrMsvHzH6a3ruCDty"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
